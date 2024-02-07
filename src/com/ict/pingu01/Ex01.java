6c40ed2a4263fb483e0977722f1dd9a32c6a6d8e
pingu <dudfks232031@naver.com>
2024-02-08 01:02:14 +0900

    home

---
+++ src/com/ict/pingu01/Ex01.java
@@ -0,0 +1,25 @@
+package com.ict.pingu01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		/*
+				
+		버림(권고) = trunc
+
+		제곱승하는거 = power
+
+		버림 = floor
+		 
+		올림 = cell
+		
+		반올림 = round
+
+		절대값 = abs 
+		
+		나머지값출력 = mod 
+
+		음수 -1, 양수 1 = sign 
+				
+		*/		
+	}
+}
